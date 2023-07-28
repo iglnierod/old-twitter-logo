48512c9af729069b8ba8a4bc0d298b39955c745e
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-07-28 14:18:47 +0200

    Edited comments

--- content_script.js
+++ content_script.js
@@ -2,8 +2,7 @@
 
 // Verificar si estamos en el dominio de Twitter
 const isTwitterDomain = /twitter\.com$|\.twitter\.com$/.test(window.location.hostname);
-
-// URL de la imagen personalizada (logo.png) en GitHub
+// Logo de twitter
 const customLogoUrl = "https://pbs.twimg.com/media/F2C2gcKWYAAaaOS?format=png&name=small";
 
 // Cambiar el favicon si estamos en el dominio de Twitter
@@ -11,5 +10,4 @@ if (isTwitterDomain) {
   // Obtener el elemento link del favicon
   const faviconLink = document.querySelector('link[rel="shortcut icon"]');
   faviconLink ? faviconLink?.setAttribute('href', customLogoUrl) : null;
-
 }
